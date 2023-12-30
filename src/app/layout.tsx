47e86bcf0147f8cc7df7238c009b6fcd103614ec
Steve Goode <stevei.goode@nearform.com>
2023-12-30 12:14:41 +0000

    Tidied up logos. Removed Twitch embed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import './globals.css'
 
 import logo from '../../public/logo.png';
+import twitch from '../../public/twitch.png';
+import discord from '../../public/discord-logo-black.png';
+import etsy from '../../public/etsy-logo-bar-black.png';
 import Link from 'next/link';
 
 const DEFAULT_FONT = Roboto({ subsets: ['latin'], weight: '400' });
@@ -12,6 +15,8 @@ export const metadata: Metadata = {
   title: 'Auroras Madhouse',
   description: 'Purchase diamond painting accessories, see Auroras projects, help her decide on the next project'
 }
+
+const logoHeight = 40;
 
 export default function RootLayout({
   children,
@@ -22,12 +27,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${DEFAULT_FONT.className}`}>
         <nav>
-          <Link href='/'>
-            <Image src={logo} alt="Auroras Madhouse Logo - A white unicorn with a blue mane and purple tail sat on a stool holding a cup of tea with the words Auroras Madhouse below" height={200} />
-          </Link>
-          <Link href='/'>Help choose our next project</Link>
-          <Link href='https://aurorasmadhouse.etsy.com' target='_etsy'>Visit our Etsy Store</Link>
-          <Link href='/past-projects'>See Past Projects</Link>
+          <Image src={logo} alt="Auroras Madhouse Logo - A white unicorn with a blue mane and purple tail sat on a stool holding a cup of tea with the words Auroras Madhouse below" height={200} />
+          <div className='logo-strip'>
+            <Link href='https://aurorasmadhouse.etsy.com'>Visit our Store on <Image src={etsy} height={logoHeight} alt="Etsy Logo"  /></Link>
+            <Link href='https://twitch.tv/aurorasmadhouse'>Join me on <Image src={twitch} height={logoHeight} alt="Twitch Logo" /></Link>
+            <Link href='https://discord.gg/CWQ6xpTT2Q'>Chat with us on <Image src={discord} height={logoHeight} alt="Discord Logo" /></Link>
+          </div>
         </nav>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-import Link from 'next/link'
-import twitch from '../../public/twitch.png'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className={`grid grid-cols-2 auto-cols-auto gap-5`}>
-      <p>
-        I stream on Twitch every Monday, Wednesday, and Friday at 18:30 UK time.
-        <Link href='http://timebie.com/std/london.php?q=18.5'>Check what time that is for you here.</Link>
-
-        <Link href='https://twitch.tv/AurorasMadhouse/'><Image src={twitch} alt='Twitch Logo' /></Link>
-      </p>
-      <iframe
-        className={`aspect-video w-full`}
-        src="https://player.twitch.tv/?AurorasMadhouse&parent=aurorasmadhouse.com" />
+    <main>
     </main>
   )
 }
