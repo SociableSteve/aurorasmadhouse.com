dcfd3367443a84e803a87f6c5dfe25bc4c7d11a4
Steve Goode <stevei.goode@nearform.com>
2024-01-01 20:15:58 +0000

    priority load current project

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -6,7 +6,7 @@ export default function ContentCard({project, primary}: {project: any, primary: 
             <strong className='mb-2'>{project.fields.title}</strong>
             {project.fields.dateStarted && (<><br /> Started: {new Date(project.fields.dateStarted).toDateString()}</>)}
             {project.fields.dateCompleted && (<><br /> Started: {new Date(project.fields.dateCompleted).toDateString()}</>)}
-            <Image src={`https:${project.fields.image.fields.file.url}`} alt={project.fields.title} height={500} width={500} className='mt-2 mx-auto' />
+            <Image src={`https:${project.fields.image.fields.file.url}`} alt={project.fields.title} height={500} width={500} className='mt-2 mx-auto' priority={primary} />
         </div>
     )
 }
