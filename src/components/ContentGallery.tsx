bfb807bc791fa538b22a9be03314bd16d880916c
Steve Goode <stevei.goode@nearform.com>
2024-01-01 19:58:11 +0000

    Made content gallery more responsive

--- src/components/ContentGallery.tsx
+++ src/components/ContentGallery.tsx
@@ -2,7 +2,7 @@ import ContentCard from "./ContentCard";
 
 export default function ContentGallery({projects} : {projects: any}) {
     return (
-        <div className='grid grid-cols-4 gap-10'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-4'>
             {projects.map((project: any) => <ContentCard key={project.sys.id} project={project} primary={false} />)}
         </div>
     );
